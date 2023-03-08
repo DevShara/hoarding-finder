04c0726b5c2df9854efd5288ff5530e7c8da217b
Devshara <developer.ishara@gmail.com>
2023-03-08 01:38:38 +0000

    getCityList from the API

--- src/components/SearchPanel.jsx
+++ src/components/SearchPanel.jsx
@@ -1,6 +1,31 @@
-import "../styles/index.css"
+import { useEffect } from "react";
+import "../styles/index.css";
+
+
+let CITIES ;
 
 const SearchPanel = ({searchHoardings}) => {
+
+    useEffect(() => {
+      getCityList()
+    }, []);
+
+    async function getCityList(){
+      let PROJECT_ID = "11j4bpx0";
+      let DATASET = "production";
+    
+      let URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${encodeURIComponent("*[_type == 'hoarding']{city}")}`;
+  
+      const apiRes = await fetch(URL);
+      const res = await apiRes.json();
+  
+     
+      CITIES = res.result
+
+      console.log(CITIES);
+  
+    }
+
     return(
       <div className="bg-gray-600" >
           <div className="container text-white  md:w-1/3 mx-auto p-8 flex flex-col gap-4 justify-center items-center">
@@ -26,11 +51,15 @@ const SearchPanel = ({searchHoardings}) => {
               <label htmlFor="city" >
                 City
                 <select name="city" id="city" className="w-full mt-1 text-gray-600" >
-                  <option value="Kurunegala">Kurunegala</option>
-                  <option value="Kandy">Kandy</option>
-                  <option value="Mawathagama">Mawathagama</option>
-                  <option value="Katugastota">Katugastota</option>
-                  <option value="Matale">Matale</option>
+
+
+                  { CITIES &&
+                    CITIES.map(hoarding => {
+                      return(
+                        <option key={hoarding.city} value={hoarding.city}>{hoarding.city}</option>
+                      )
+                    })
+                  }                 
                 </select>
               </label>
 
