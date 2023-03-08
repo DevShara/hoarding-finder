88aabbd8e0aef38997013d9ea6b9af62046f6a74
Devshara <developer.ishara@gmail.com>
2023-03-08 16:36:10 +0000

    style available & not available buttons

--- src/components/Hoarding.jsx
+++ src/components/Hoarding.jsx
@@ -7,7 +7,7 @@ const Hoarding = ({hoarding}) => {
             <Link to={`/details/${hoarding._id}`}>
             <div className="flex flex-row container  mx-auto md:w-1/2  gap-6  p-3 justify-start ">
                 <h2 className=" text-xl">{hoarding.location} - {hoarding.size}  - {hoarding.route}</h2>
-                <div className=" text-sm border-lime-700 bg-lime-100 border p-1 font-semibold text-lime-700  ">{hoarding.isAvailable ? " Available" : " Not Available"}</div>
+                <div className={ ` text-sm p-1 border ${hoarding.isAvailable ? "  border-lime-700 bg-lime-100   text-lime-700" : "  border-red-700 bg-red-100   text-red-700"}  `}>{hoarding.isAvailable ? " Available" : " Not Available"}</div>
             </div>
              
             </Link>
